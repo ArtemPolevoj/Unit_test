3754cb5d5096e5ad4ec66f1314530296c83fe632
Артём <artempolevoj@yandex.ru>
2023-09-19 15:11:18 +0300

    added tests in CalculatorTest

--- src/Calculator/CalculatorTest.java
+++ src/Calculator/CalculatorTest.java
@@ -1,5 +1,7 @@
 package Calculator;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class CalculatorTest {
     public static void main(String[] args) {
         // Проверка базового функционала с целыми числами:
@@ -21,13 +23,13 @@ public class CalculatorTest {
 
         // Случаи с неправильными аргументами
         // аргумент operator типа char, должен вызывать исключение, если он получает не базовые символы (+-*/)
-         try {
-             Calculator.calculation(8, 4, '_');
-         } catch (IllegalStateException e) {
-             if (!e.getMessage().equals("Unexpected value operator: _")) {
-                 throw new AssertionError("Ошибка в методе");
-             }
-         }
+        try {
+            Calculator.calculation(8, 4, '_');
+        } catch (IllegalStateException e) {
+            if (!e.getMessage().equals("Unexpected value operator: _")) {
+                throw new AssertionError("Ошибка в методе");
+            }
+        }
 
         // Проверка базового функционала с целыми числами, с использованием утверждений:
         assert 8 == Calculator.calculation(2, 6, '+');
@@ -35,6 +37,16 @@ public class CalculatorTest {
         assert 14 == Calculator.calculation(2, 7, '*');
         assert 2 == Calculator.calculation(100, 50, '/');
 
-
+//Ваша задача - проверить этот метод с использованием библиотеки AssertJ
+        assertThatThrownBy(() -> Calculator.calculatingDiscount(0, 10)
+        ).isInstanceOf(ArithmeticException.class);
+        assertThatThrownBy(() -> Calculator.calculatingDiscount(200, -1)
+        ).isInstanceOf(ArithmeticException.class);
+        assertThatThrownBy(() -> Calculator.calculatingDiscount(200, 101)
+        ).isInstanceOf(ArithmeticException.class);
+        assertThat(Calculator.calculatingDiscount(100, 10)).isEqualTo(90);
+        assertThat(Calculator.calculatingDiscount(100, 0)).isEqualTo(100);
+        assertThat(Calculator.calculatingDiscount(100, 100)).isEqualTo(0);
+        assertThat(Calculator.calculatingDiscount(100, 50)).isEqualTo(50);
     }
 }
