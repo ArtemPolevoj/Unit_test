9faf9f24188d33be0941beb911ed881280c1ea87
Артём <artempolevoj@yandex.ru>
2023-09-24 08:58:15 +0300

    added setUpp and catInstanceOfCat

--- Test/seminars/second/hw/VehicleTest.java
+++ Test/seminars/second/hw/VehicleTest.java
@@ -1,9 +1,31 @@
 package seminars.second.hw;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import secondSeminar.hw.Car;
+import secondSeminar.hw.Motorcycle;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
+    static Car car;
+    static Motorcycle motorcycle;
+
+    @BeforeAll
+    static void setUpp() {
+        car = new Car("Company", "Model", 5);
+        motorcycle = new Motorcycle("Company", "Model", 5);
+    }
+
+    /**
+     * Проверить, что экземпляр объекта Car также является экземпляром
+     * транспортного средства (используя оператор instanceof).
+     */
+    @Test
+    void catInstanceOfCar() {
+        assertTrue(car instanceof Car);
+
+    }
+
 
 }
