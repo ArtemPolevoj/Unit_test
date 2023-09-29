543a4478e72ea8d30686d6aefe0d01311b4425b1
artem <artempolevoj@yandex.ru>
2023-09-29 07:48:55 +0300

    added third seminar package

---
+++ Test/seminars/third/tdd/TestDrivenDevelopment.java
@@ -0,0 +1,10 @@
+package seminars.third.tdd;
+
+import org.junit.jupiter.api.Test;
+
+
+
+public class TestDrivenDevelopment {
+
+    //3.5. Red-Green-Refactor
+}
---
+++ src/thirdSeminar/tdd/MoodAnalyser.java
@@ -0,0 +1,9 @@
+package thirdSeminar.tdd;
+
+public class MoodAnalyser {
+
+    public String analyseMood(String message) {
+        return "";
+    }
+
+}
