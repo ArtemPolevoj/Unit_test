3e0fc795dceba9466c4d304461a6b1d189ab5337
Артём <artempolevoj@yandex.ru>
2023-10-07 14:52:48 +0300

    update TestHwList

--- Test/seminars/sixth/TestHwList.java
+++ Test/seminars/sixth/TestHwList.java
@@ -2,35 +2,38 @@ package seminars.sixth;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import secondSeminar.hw.Car;
 import sixth.HwList;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestHwList {
-     private ArrayList<Integer> temp;
+    private ArrayList<Integer> temp;
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         temp = new HwList().getRandomInteger(50, 1, 3);
     }
+
     @Test
-    void checkClass(){
+    void checkClass() {
         assertTrue(temp instanceof ArrayList<Integer>);
     }
+
     @Test
-    void checkMin(){
-        assertEquals(Collections.min(temp),1);
+    void checkMin() {
+        assertTrue(Collections.min(temp) >= 1);
     }
+
     @Test
-    void checkMax(){
-        assertEquals(Collections.max(temp),2);
+    void checkMax() {
+        assertTrue(Collections.max(temp) < 3);
     }
+
     @Test
-    void checkSize(){
-        assertEquals(temp.size(),50);
+    void checkSize() {
+        assertEquals(temp.size(), 50);
     }
 }
